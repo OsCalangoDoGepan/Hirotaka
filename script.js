fceb6994da241d94475955e98102c92b19878f47
OsCalangoDoGepan <oscalangodogepan@gmail.com>
2025-02-11 22:52:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.getElementById("loading-screen").style.display = "none";
+    }, 2000); // Tela de loading some após 2 segundos
+});
